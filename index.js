b8befaf070cfc85de76a142585904ae22eded55f
ButteredToast01 <isyed0@icloud.com>
2025-09-29 10:41:32 +0000

    btn 2

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2")
 const screen = document.getElementById("screen");
 
 btn1.addEventListener("click", function() {
     screen.textContent += "1";
 });
+
+btn2.addEventListener("click", function() {
+    screen.textContent += "2";
+})
